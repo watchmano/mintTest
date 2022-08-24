dce7e23b202da33397b58fd7f1a3f8d6c78691fe
Young Jung <mark.jung.0918@gmail.com>
2022-08-24 13:42:35 +0900

    Init commit

---
+++ dapp.config.js
@@ -0,0 +1,10 @@
+const config = {
+  title: '360 Hexaworld 3D Models',
+  description: 'Designed by Hexaworld Users, Assembled by 360Hexaworld',
+  contractAddress: '0xa06A1D956531636ef342C119F9F1793eD5c69436',
+  maxMintAmount: 100,
+  presaleMaxMintAmount: 3,
+  price: 0.0001
+}
+
+export { config }
